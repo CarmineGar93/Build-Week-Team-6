70c617b3d80cf8daf9e63662216a74caa07209e4
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-11 12:02:46 +0100

    Initial commit

---
+++ src/test/java/Team6/Build_Week_Team_6/BuildWeekTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package Team6.Build_Week_Team_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekTeam6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
