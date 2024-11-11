2f5033b12054fd902e80f04c0d1156d9bfab0519
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-11 12:02:46 +0100

    Initial commit

---
+++ src/main/java/Team6/Build_Week_Team_6/BuildWeekTeam6Application.java
@@ -0,0 +1,13 @@
+package Team6.Build_Week_Team_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekTeam6Application.class, args);
+	}
+
+}
