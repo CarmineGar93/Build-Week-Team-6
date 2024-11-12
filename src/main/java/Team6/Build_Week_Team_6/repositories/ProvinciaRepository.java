0301dc81e682f09c4f24621725a34896a3e7dc88
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-12 09:17:00 +0100

    inizializzazione db quasi conclusa con comuni e province

--- src/main/java/Team6/Build_Week_Team_6/repositories/ProvinciaRepository.java
+++ src/main/java/Team6/Build_Week_Team_6/repositories/ProvinciaRepository.java
@@ -3,7 +3,12 @@ package Team6.Build_Week_Team_6.repositories;
 import Team6.Build_Week_Team_6.entities.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProvinciaRepository extends JpaRepository<Provincia, UUID> {
+    Optional<Provincia> findByNome(String nome);
+
+    List<Provincia> findByNomeStartingWithIgnoreCase(String partial);
 }
---
+++ src/main/java/Team6/Build_Week_Team_6/services/ComuniService.java
@@ -0,0 +1,22 @@
+package Team6.Build_Week_Team_6.services;
+
+import Team6.Build_Week_Team_6.entities.Comune;
+import Team6.Build_Week_Team_6.repositories.ComuneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ComuniService {
+    @Autowired
+    private ComuneRepository comuneRepository;
+
+    public List<Comune> findAllComuni() {
+        return comuneRepository.findAll();
+    }
+
+    public Comune saveComune(Comune comune) {
+        return comuneRepository.save(comune);
+    }
+}
