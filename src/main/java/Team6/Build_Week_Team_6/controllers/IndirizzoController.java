c1be82d1586796bb7e7369d7037503eb6448abdd
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-12 14:50:50 +0100

    merge delle branches e update erd

--- src/main/java/Team6/Build_Week_Team_6/controllers/IndirizzoController.java
+++ src/main/java/Team6/Build_Week_Team_6/controllers/IndirizzoController.java
@@ -2,13 +2,16 @@ package Team6.Build_Week_Team_6.controllers;
 
 import Team6.Build_Week_Team_6.dto.IndirizzoDTO;
 import Team6.Build_Week_Team_6.dto.IndirizzoResponseDTO;
+import Team6.Build_Week_Team_6.exceptions.BadRequestException;
 import Team6.Build_Week_Team_6.services.IndirizzoService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/indirizzi")
@@ -18,7 +21,13 @@ public class IndirizzoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public IndirizzoResponseDTO save(@RequestBody @Valid IndirizzoDTO body) {
+    public IndirizzoResponseDTO save(@RequestBody @Validated IndirizzoDTO body, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String message =
+                    bindingResult.getAllErrors().stream().map(s -> s.getDefaultMessage()).collect(Collectors.joining(
+                            ", "));
+            throw new BadRequestException(message);
+        }
         return indirizzoService.save(body);
     }
 
