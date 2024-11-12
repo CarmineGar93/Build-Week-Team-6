647832ce0eecac368cc2fe9b6d3d1849588babba
Gabriele <gabrielegnr@gmail.com>
2024-11-12 17:46:49 +0100

    modificato ProvinciaController

--- src/main/java/Team6/Build_Week_Team_6/controllers/ProvinciaController.java
+++ src/main/java/Team6/Build_Week_Team_6/controllers/ProvinciaController.java
@@ -1,11 +1,11 @@
 package Team6.Build_Week_Team_6.controllers;
 
-import Team6.Build_Week_Team_6.dto.ProvinciaDTO;
+import Team6.Build_Week_Team_6.entities.Provincia;
 import Team6.Build_Week_Team_6.services.ProvinciaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/province")
 public class ProvinciaController {
+
     @Autowired
     private ProvinciaService provinciaService;
 
     @GetMapping
-    public ResponseEntity<List<ProvinciaDTO>> getAllProvince() {
-        List<ProvinciaDTO> province = provinciaService.getAllProvince();
-        return ResponseEntity.ok(province);
+    public List<Provincia> getAllProvince(@RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "10") int size,
+                                        @RequestParam(defaultValue = "asc") String sortBy) {
+        return provinciaService.findAllProvince();
     }
 }
