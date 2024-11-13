11383472959bc65fbdb7cee3daaa9004c57b6989
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-13 08:37:34 +0100

    soluzione trovata al filtraggio multiplo usando l'interfaccia specification

--- src/main/java/Team6/Build_Week_Team_6/repositories/ClienteRepository.java
+++ src/main/java/Team6/Build_Week_Team_6/repositories/ClienteRepository.java
@@ -2,13 +2,11 @@ package Team6.Build_Week_Team_6.repositories;
 
 import Team6.Build_Week_Team_6.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
-public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+public interface ClienteRepository extends JpaRepository<Cliente, UUID>, JpaSpecificationExecutor<Cliente> {
     boolean existsByPartitaIva(String partitaIva);
 
     boolean existsByRagioneSociale(String ragioneSociale);
@@ -18,13 +16,5 @@ public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
     boolean existsByPec(String pec);
 
     boolean existsByTelefono(String telefono);
-
-    @Query("SELECT c FROM Cliente c WHERE " +
-            "(:fatturatoAnnuale IS NULL) OR (c.fatturatoAnnuale > :fatturatoAnnuale) AND " +
-            "(:dataInserimento IS NULL) OR (c.dataInserimento = :dataInserimento) AND " +
-            "(:dataUltimoContatto IS NULL) OR (c.dataUltimoContatto = :dataUltimoContatto) AND " +
-            "(:ragioneSociale IS NULL) OR (c.ragioneSociale LIKE :ragioneSociale)")
-    List<Cliente> filtriCustom(Double fatturatoAnnuale, LocalDate dataInserimento, LocalDate dataUltimoContatto,
-                               String ragioneSociale);
 
 }
