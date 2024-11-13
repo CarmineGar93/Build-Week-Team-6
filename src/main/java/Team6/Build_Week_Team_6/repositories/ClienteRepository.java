e648112d27a81fb58fe29cd2642c202642762f15
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-13 09:01:57 +0100

    aggiunta fonte

--- src/main/java/Team6/Build_Week_Team_6/repositories/ClienteRepository.java
+++ src/main/java/Team6/Build_Week_Team_6/repositories/ClienteRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.UUID;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID>, JpaSpecificationExecutor<Cliente> {
+    //https://medium.com/@danaprata/how-to-master-the-art-of-multi-parameter-search-98305ee71969
     boolean existsByPartitaIva(String partitaIva);
 
     boolean existsByRagioneSociale(String ragioneSociale);
