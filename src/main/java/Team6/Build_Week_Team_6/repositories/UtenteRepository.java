58ec209097a1bcd0e7eb2c7f3c4d47ab25fb734b
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-13 16:34:51 +0100

    utente dto e utente login dto creati

--- src/main/java/Team6/Build_Week_Team_6/repositories/UtenteRepository.java
+++ src/main/java/Team6/Build_Week_Team_6/repositories/UtenteRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UtenteRepository extends JpaRepository<Utente, UUID> {
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
